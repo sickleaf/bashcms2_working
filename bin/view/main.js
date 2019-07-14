6fd9af55b22b383110612d0bf0ec7156948d81c8
sickleaf <5824061+sickleaf@users.noreply.github.com>
2019-07-14 21:06:14 +0900

    [add/rev] 5.3.1

--- bin/view/main.js
+++ bin/view/main.js
@@ -1,5 +1,6 @@
 window.onload = function () {
     lastArticles(10); 
+    linkKeywords();
 }                     
                       
 function lastArticles(num){
@@ -14,4 +15,19 @@ function lastArticles(num){
     httpReq.open("GET",url,true);   
     httpReq.send(null);             
 }
+
+function linkKeywords(){
+    var httpReq = new XMLHttpRequest();
+    httpReq.onreadystatechange = function(){
+        if(httpReq.readyState != 4 || httpReq.status != 200)
+            return;                 
+
+        document.getElementById("keywords").innerHTML = httpReq.responseText;
+    }                               
+    var word = document.getElementById("keywords").innerHTML;
+    var url = "/link_keywords.cgi?keywords=" + encodeURIComponent(word);
+    httpReq.open("GET",url,true);   
+    httpReq.send(null);             
+}
+
 
